effa7411b18f66dc2b566b3799e69755136764ae
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-01-27 09:37:40 -0600

    able to see registration page

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,19 @@ const router = express.Router()
 const User = require('../models/user')
 
 //registration 
+router.get('/register', (req, res) => {
+  console.log("\n able to register");
+  console.log(req.session.message);
 
+  let messageToDisplay = req.session.message
+  let messageStatus = req.session.messageStatus
 
+  req.session.message = undefined
+
+  res.render('register.ejs', {
+    message: messageToDisplay,
+  })
+})
 
 
 
